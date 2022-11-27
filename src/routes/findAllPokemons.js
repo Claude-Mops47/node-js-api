bbbb75dbb4285b8e38445907f34b067b481dee32
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-27 16:34:11 +0100

    les contraintes de recherche

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -6,6 +6,13 @@ module.exports = (app) => {
     // recherche par name :
     if (req.query.name) {
       const name = req.query.name;
+      const limit = parseInt(req.query.limit) || 5;
+
+      if (name.length < 2) {
+        const message =
+          "Le terme de recherche doit contenir au moins 2 caractères.";
+        return res.status(400).json({ message });
+      }
       return Pokemon.findAndCountAll({
         where: {
           name: {
@@ -14,14 +21,15 @@ module.exports = (app) => {
           },
         },
         // la limite de la recherche
-        limit: 5,
+        limit: limit,
+        order: ["name"],
       }).then(({ count, rows }) => {
         const message = `Il y a ${count} pokemons qui correspondent au terme de recherche ${name}`;
         res.json({ message, data: rows });
       });
     } else {
       // recupéré tout les pokemons dans la base de données
-      Pokemon.findAll()
+      Pokemon.findAll({ order: ["name"] })
         .then((pokemons) => {
           const message = "La liste des pokémons a bien été récupérée.";
           res.json({ message, data: pokemons });
